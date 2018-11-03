3b591b6b560f5470a87952898ac711b2c6fee714
Julien Vienne <vienne.julien@gmail.com>
2018-11-03 09:44:27 +0100

    Changement port par défaut + test health check

---
+++ src/test/java/com/nexity/wgl/ms/fiches/location/ServiceFichesLocationActuatorTest.java
@@ -0,0 +1,35 @@
+package com.nexity.wgl.ms.fiches.location;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ServiceFichesLocationActuatorTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void testHealthShouldReturnStatusOK() throws Exception {
+		final ResponseEntity<String> response = restTemplate.getForEntity(appUrl() + "health", String.class);
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+	private String appUrl() {
+		return "http://localhost:" + port + "/";
+	}
+}
